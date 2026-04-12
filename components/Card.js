a726f182ef94ef0327875cad8c9ee04d2d5ebc73
Degen-eth <ogehprosper@gmail.com>
2026-04-12 05:04:03 -0700

    Center ethereum logos in tokenomics cards and exchanges

--- components/Card.js
+++ components/Card.js
@@ -15,7 +15,7 @@ const Card = ({ props: { img, title, desc } }) => {
               width={500}
               height={500}
               unoptimized
-              className="h-full w-full object-contain"
+              className="h-2/3 w-auto object-contain"
             />
           </div>
 
